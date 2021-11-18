198d1ddecb15ea826376bd8d5d784e673375805f
CarlosOrtiz <carlos.ortiz@udla.edu.co>
2021-11-17 22:33:07 -0500

    fix: useState

--- src/components/BadgeForm.js
+++ src/components/BadgeForm.js
@@ -1,34 +1,97 @@
-import React from "react"
+import React, { useState } from "react"
+const log = console.log;
 
-const BadgeForm = () => {
+const BadgeForm = (props) => {
+
+  const [form, setValues] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    jobTitle: "",
+    twitter: ""
+  });
 
   const handleChange = e => {
-    console.log({
-      name: e.target.name,
-      value: e.target.value
-    })
-  };
+    setValues({
+      ...form,
+      [e.target.name]: e.target.value
+    });
+  }
 
   const handleClick = () => {
-    console.log('Buttooooooo')
+    log('Buttooooooo')
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    log('Form was submitted');
+    log(form);
   }
 
   return (
     <div>
       <h1>New Attendant</h1>
 
-      <form>
+      <form onSubmit={handleSubmit}>
+
         <div className='form-group'>
-          <label>First Name</label>
+          <label>First Name {form.firstName} {props.firstName}</label>
           <input
             onChange={handleChange}
             className="form-control"
             type="text"
             name="firstName"
+            value={props.firstName}
           />
         </div>
 
-        <button type="button" onClick={handleClick} className="btn btn-primary">Save</button>
+        <div className='form-group'>
+          <label>Last Name</label>
+          <input
+            onChange={handleChange}
+            className="form-control"
+            type="text"
+            name="lastName"
+            value={props.lastName}
+          />
+        </div>
+
+        <div className='form-group'>
+          <label>Email</label>
+          <input
+            onChange={handleChange}
+            className="form-control"
+            type="email"
+            name="email"
+            value={props.email}
+          />
+        </div>
+
+        <div className='form-group'>
+          <label>Job</label>
+          <input
+            onChange={handleChange}
+            className="form-control"
+            type="text"
+            name="jobTitle"
+            value={props.jobTitle}
+          />
+        </div>
+
+        <div className='form-group'>
+          <label>Twitter</label>
+          <input
+            onChange={handleChange}
+            className="form-control"
+            type="text"
+            name="twitter"
+            value={props.twitter}
+          />
+        </div>
+
+        <button onClick={handleClick} className="btn btn-primary">
+          Save
+        </button>
       </form>
     </div>
   );
--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import './styles/BadgeNew.css'
 import header from "../images/badge-header.svg";
@@ -8,6 +8,27 @@ import Badge from '../components/Badge';
 import BadgeForm from '../components/BadgeForm';
 
 const BadgeNew = () => {
+  //HOOK
+  const [form, setValues] = useState({
+    form: {
+      firstName: "",
+      lastName: "",
+      email: "",
+      jobTitle: "",
+      twitter: ""
+    }
+  });
+
+  const handleChange = e => {
+    console.log(e)
+    setValues({
+      form: {
+        ...form ,
+        props: { [e.target.name]: e.target.value }
+      },
+    });
+  }
+
   return (
     <div>
       <Navbar />
@@ -18,17 +39,20 @@ const BadgeNew = () => {
       <div className="container">
         <div className="row">
 
-          <div className="col">
+          <div className="col-6">
+            <div>X:{form.form.firstName}</div>
+
             <Badge
-              name='Carlos Alberto'
+              name={form.form.firstName}
               lastName='Ortiz Losada'
               jobTitle='Frontend Developer'
               photo="https://www.gravatar.com/avatar/7b083bcd16a7e6f6b262fd7b6ca4eeac?d=identicon"
               twitter='CarlosOrtizL' />
           </div>
 
-          <div className="col">
-            <BadgeForm />
+          <div className="col-6">
+            {console.log(form)}
+            <BadgeForm onChange={handleChange} formValues={form} />
           </div>
 
         </div>
